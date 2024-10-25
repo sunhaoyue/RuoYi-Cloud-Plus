96364f0d3b249237c8505993b1d2c28a7d15b2d8
疯狂的狮子Li <15040126243@163.com>
2024-10-25 13:10:31 +0800

    fix 修复 注册日志记录状态错误

--- ruoyi-auth/src/main/java/org/dromara/auth/service/SysLoginService.java
+++ ruoyi-auth/src/main/java/org/dromara/auth/service/SysLoginService.java
@@ -175,11 +175,11 @@ public class SysLoginService {
         String captcha = RedisUtils.getCacheObject(verifyKey);
         RedisUtils.deleteObject(verifyKey);
         if (captcha == null) {
-            recordLogininfor(tenantId, username, Constants.REGISTER, MessageUtils.message("user.jcaptcha.expire"));
+            recordLogininfor(tenantId, username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.expire"));
             throw new CaptchaExpireException();
         }
         if (!code.equalsIgnoreCase(captcha)) {
-            recordLogininfor(tenantId, username, Constants.REGISTER, MessageUtils.message("user.jcaptcha.error"));
+            recordLogininfor(tenantId, username, Constants.LOGIN_FAIL, MessageUtils.message("user.jcaptcha.error"));
             throw new CaptchaException();
         }
     }
