591db0bdd27db2c9e84d033ada3c4b05ada3da5e
疯狂的狮子Li <15040126243@163.com>
2024-11-23 02:36:53 +0000

    update 优化 个人中心强退接口路径
    Signed-off-by: 疯狂的狮子Li <15040126243@163.com>

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/controller/monitor/SysUserOnlineController.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/controller/monitor/SysUserOnlineController.java
@@ -112,7 +112,7 @@ public class SysUserOnlineController extends BaseController {
      * @param tokenId token值
      */
     @Log(title = "在线设备", businessType = BusinessType.FORCE)
-    @PostMapping("/{tokenId}")
+    @DeleteMapping("/myself/{tokenId}")
     public R<Void> remove(@PathVariable("tokenId") String tokenId) {
         try {
             // 获取指定账号 id 的 token 集合
